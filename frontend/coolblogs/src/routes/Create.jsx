f56961311fc983ee7278189b1a73a29ac48afcf3
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2023-04-19 06:54:43 -0400

    Removed unused Link Component

--- frontend/coolblogs/src/routes/Create.jsx
+++ frontend/coolblogs/src/routes/Create.jsx
@@ -11,7 +11,6 @@ import {
   Input,
   useToast,
 } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
 import useGlobalReducer from '../utils/useGlobalReducer';
 
 function Create() {
